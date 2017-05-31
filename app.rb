9d5badadc9b165d90bc11acd5f9a5a52a795444c
Avidor Turkewitz <avidor@flatironschool.com>
2017-05-31 11:24:35 -0400

    Update port var in app.rb

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 # Uncomment the two lines below if you're on the Learn IDE!
 # set :bind, ENV["HOST_IP"]
-# set :port, ENV["HOST_PORT"]
+# set :port, ENV["HTTP_PORT"]
 
 get '/' do
   "Hello, World!"
